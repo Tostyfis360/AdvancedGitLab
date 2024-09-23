94e255e1884e72c4da8cc9b1624b1b3a8a741506
Juan Ferrera Sala <tostyfis360@gmail.com>
2024-09-23 17:36:39 +0200

    Task 2 add greeting

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,20 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+        public static void main(String[] args) {
+        System.out.println("Introduce your name");
+
+        String name = inputUserString();
+
+        System.out.println("Hello: " + name + " !!!");
+    }
+
+    public static String inputUserString() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        return input;
     }
 }
