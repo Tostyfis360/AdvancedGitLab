ea5625bbcb6a2ee4ad5b6eab0e56a1b1f571e1aa
Juan Ferrera Sala <tostyfis360@gmail.com>
2024-09-23 17:41:59 +0200

    Task 3  add date

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,11 @@
 package com.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Main {    public static void main(String[] args) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+        LocalDateTime now = LocalDateTime.now();  
+        System.out.println(dtf.format(now));
     }
 }
